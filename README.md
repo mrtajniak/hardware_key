32db549c7ceea2e0802f1e8a9d7968395b9b30fd
Krystian <33846499+mrtajniak@users.noreply.github.com>
2023-01-14 02:04:46 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# Wstęp
+Aktywator oparty na kluczu sprzętowym podobnym do iLok stworzony za pomocą Pythona
+
+# Wymagania
+## System operacyjny
+1. macOS High Sierra (10.13) lub nowszy
+2. Windows 10 20H2 lub nowszy (skrypt nie został przetestowany)
+
+## Wymagana wersja Python i wszystkie potrzebne biblioteki
+Python:
+- Python 3.11 lub nowszy
+
+Biblioteki:
+- pyUSB
+- pyOpenSSL
+- crypto
+
+## Jak rozpocząć pracę
+1. Skolnuj repozyroium na swój komputer za pomocą Visual Studio lub Visual Studio Code
+2. Uruchum polecenie `pip install -r requirements.txt`
+3. Otwórz plik python w wybranym IDE aby go edytować lub przetestować
+4. **WAŻNE: Na potrzebny testów należy zmienić numer seryjny dysku USB**
---
+++ manager.py
@@ -0,0 +1,37 @@
+import os
+from OpenSSL import crypto
+import usb
+
+usb_serial_number = '0A7205079090'
+
+# Iterate over all connected USB devices
+for device in usb.core.find(find_all=True):
+    # Get the serial number of the USB device
+    serial_number = device.serial_number
+    if serial_number == usb_serial_number:
+        # Get the device file associated with the USB device
+        device_file = usb.util.get_string(device, device.iSerialNumber)
+        # Search for the certificate file in the root directory of the USB stick
+        for root, dirs, files in os.walk(device_file):
+            for file in files:
+                if file.endswith(".pem"):
+                    key_file = os.path.join(root, file)
+                    break
+        break
+
+if key_file and os.path.exists(key_file):
+    with open(key_file, 'rb') as f:
+        cert_data = f.read()
+    cert = crypto.load_certificate(crypto.FILETYPE_PEM, cert_data)
+    store = crypto.X509Store()
+    store.add_cert(cert)
+    store_ctx = crypto.X509StoreContext(store, cert)
+    # Verify the certificate using the store
+    if store_ctx.verify_certificate() == None:
+        print("Certificate is valid")
+        # Allow the software to run
+    else:
+        # The certificate is not valid, prevent the software from running
+        print("Invalid certificate")
+else:
+    print("Certificate not found on USB stick")
